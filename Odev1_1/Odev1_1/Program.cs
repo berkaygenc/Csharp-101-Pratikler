b0348c6fbc775582a7ce61daafc05a475d5366bd
Berkay Genç <92612497+berkaygenc@users.noreply.github.com>
2022-12-10 14:24:10 +0300

    Odev-1
    algorithm questions added.

---
+++ Odev1_1/Odev1_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Odev1_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen pozitif bir sayı girin: ");
+            int n = Int32.Parse(Console.ReadLine());
+            int[] dizi=new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write((i+1)+"."+" pozitif sayiyi girin: ");
+                    dizi[i] =Int32.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("-------- Çift Sayılar --------");
+            foreach (var item in dizi)
+            {
+                if (item % 2 == 0)
+                    Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Odev1_2/Odev1_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Odev1_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kaç sayı girmek istediğinizi girin (n): ");
+            int n = Int32.Parse(Console.ReadLine());
+            Console.Write("Kontrol sayısını girin (m): ");
+            int m = Int32.Parse(Console.ReadLine());
+
+            int[] dizi=new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write((i+1)+".sayıyı girin: ");
+                dizi[i]=Int32.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("-------Sonuç-------");
+            foreach (var item in dizi)
+            {
+                if(item%m==0 || item==m)
+                    Console.WriteLine(item);
+            }
+        }
+    }
+}
